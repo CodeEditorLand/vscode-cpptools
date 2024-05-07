43fdb31b3fe5dc322d6491adf60133507d408670
Nikola Hristov <nikola@playform.cloud>
2024-05-07 03:19:00 +0300

    squash!

--- Extension/Source/Utility/System/types.ts
+++ Extension/Source/Utility/System/types.ts
@@ -3,13 +3,13 @@
  * See 'LICENSE' in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
 
-/** @internal */
+
 export type Resolve<T> = (value: T | PromiseLike<T>) => void;
 
-/** @internal */
+
 export type Reject = (reason?: any) => void;
 
-/** @internal */
+
 export type ConstructorReturn<T extends abstract new (...args: any) => any> = T extends abstract new (...args: any) => infer R ? R : never;
 
 export type Primitive = string | number | boolean;
